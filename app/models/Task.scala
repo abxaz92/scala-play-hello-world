ac81dc95d31c077427d255e2bf4bc17ac53afb5f
Давид <chaava01@gmail.com>
2018-05-13 22:52:29 +0300

    anorm

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -4,8 +4,12 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import anorm.SqlParser.get
 import play.api.libs.json.Json
+import anorm._
+import play.api.db._
+import play.api.Play.current
+import anorm.SqlParser._
 
-case class Task(var id: Option[Long], label: String, who: String, mytime: String, ready: Short) {
+case class Task(var id: Long, label: String, who: String, mytime: String, ready: Short) {
 }
 
 object Task {
@@ -16,19 +20,18 @@ object Task {
       get[String]("label") ~
       get[String]("who") ~
       get[String]("mytime") ~
-      get[Int]("ready") map {
-      case id~label~who~mytime~ready => Task(id, label, who, mytime, ready)
+      get[Short]("ready") map {
+      case id~label~who~mytime~ready => new Task(id, label, who, mytime, ready)
     }
   }
 
   def apply(label: String, who: String, mytime: String, ready: Short) : Task =
-    Task(Option.empty, label, who, mytime, ready)
+    Task(0, label, who, mytime, ready)
 
   var tasks = Map[Long, Task]()
   val count = new AtomicInteger()
 
   def all(): List[Task] = tasks.values.toList.sorted(Ordering.by((task: Task) => task.id).reverse)
-  import anorm._
 
   def allDb(): List[Task] = {
     DB.withConnection { implicit connection =>
@@ -44,7 +47,7 @@ object Task {
   def create(task: Task): Unit = {
     val id = count.addAndGet(1).longValue()
     tasks += id -> task
-    task.id = Option(id)
+    task.id = id
   }
 
   def delete(id: Long) {
