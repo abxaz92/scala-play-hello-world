2f67e1b6289da89236dc23d575913920d6fa3c7d
Давид <chaava01@gmail.com>
2018-06-13 22:30:03 +0300

    actore test

--- app/async/PaymentActor.scala
+++ app/async/PaymentActor.scala
@@ -1,12 +1,13 @@
 package async
 
 import java.lang
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 
 import akka.actor.Actor
 import da.RestClient
 import play.api.Logger
 
+@Singleton
 class PaymentActor @Inject()(restClient: RestClient) extends Actor {
   val logger = Logger(this.getClass)
   override def receive = {
--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -1,29 +1,24 @@
 package controllers
 
-import java.time.{LocalDate, LocalDateTime}
-import javax.inject.Inject
+import javax.inject.{Inject, Named}
 
-import akka.actor.{ActorRef, ActorSystem, Props}
-import akka.routing.BalancingPool
-import async.PaymentActor
+import akka.actor.{ActorRef, ActorSystem}
+import akka.pattern.ask
+import akka.util.Timeout
 import da.{PaymentDao, RestClient}
-import models.{Payment, PaymentStatus, PaymentType}
 import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
-import akka.pattern.ask
-import akka.util.Timeout
 import scala.concurrent.duration._
-import scala.concurrent._
-import ExecutionContext.Implicits.global
 
-class PaymentController @Inject()(cc: ControllerComponents, paymentDao: PaymentDao, rest: RestClient) extends AbstractController(cc) {
+class PaymentController @Inject()(cc: ControllerComponents,
+                                  paymentDao: PaymentDao,
+                                  rest: RestClient,
+                                 @Named("payment-service") actor : ActorRef) extends AbstractController(cc) {
   val logger = Logger(this.getClass)
-
-  val system = ActorSystem("pay")
-
-  val actor: ActorRef = system.actorOf(Props[PaymentActor], "PaymentActor" )
 
   def getAll() = Action {
     //    val payment = new Payment(0, LocalDateTime.now(), "userId", "comment", PaymentType.Refil, 300, 1, PaymentStatus.CREATED, 0)
