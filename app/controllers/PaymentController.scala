74fe74735d8973c464d3094a42005e9973e0337e
Georgy Davityan <georgy.davityan@gmail.com>
2018-06-13 23:27:54 +0300

    refactored payment actor

--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -1,20 +1,19 @@
 package controllers
 
-import javax.inject.{Inject, Named}
-
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.ActorRef
 import akka.pattern.ask
 import akka.util.Timeout
+import async.PaymentActor
+import async.PaymentActor._
 import da.{PaymentDao, RestClient}
-import models.Paypack
+import javax.inject.{Inject, Named}
 import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent._
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
+import scala.language.postfixOps
 
 class PaymentController @Inject()(cc: ControllerComponents,
                                   paymentDao: PaymentDao,
@@ -22,31 +21,18 @@ class PaymentController @Inject()(cc: ControllerComponents,
                                   @Named("payment-service") actor: ActorRef) extends AbstractController(cc) {
   val logger = Logger(this.getClass)
 
-  def getAll() = Action {
-    //    val payment = new Payment(0, LocalDateTime.now(), "userId", "comment", PaymentType.Refil, 300, 1, PaymentStatus.CREATED, 0)
-    //    paymentDao.insert(payment)
+  implicit val timeout: Timeout = 5 seconds
+
+  def getAll = Action.async {
     val balance = paymentDao.checkBalance("userId")
     logger.info(s"user balance is '${balance}'")
 
-    val a = System.currentTimeMillis()
-    for (i <- 0 to 1) {
-      implicit val timeout = Timeout(5 seconds)
-      val resut = actor ? rest.getPaypacks()
-      resut.onComplete {
-        case Success(packs) => {
-          packs.asInstanceOf[Future[List[Paypack]]].onComplete {
-            case Success(packs) => {
-              logger.error(s"RSPONSE ${packs}")
-            }
-            case Failure(t) => logger.error(s"${t}")
-          }
-        }
-        case Failure(d) => logger.error(s"${d}")
+    (actor ? PaypacksRequest)
+      .map {
+        case PaypacksResponse(packs) =>
+          logger.error(s"Response: $packs")
+          Ok(Json.toJson(paymentDao.findAll())).as("application/json")
       }
-    }
-    val b = System.currentTimeMillis() - a
-    logger.error(s"All current millis ${b}")
-    Ok(Json.toJson(paymentDao.findAll())).as("application/json")
   }
 
 }
--- app/da/RestClient.scala
+++ app/da/RestClient.scala
@@ -1,14 +1,12 @@
 package da
 
-import java.util.concurrent.Executors
 import javax.inject.Inject
-
 import models.Paypack
 import play.api.Logger
 import play.api.libs.ws.WSClient
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
-import ExecutionContext.Implicits.global
 
 class RestClient @Inject()(ws: WSClient) {
 
