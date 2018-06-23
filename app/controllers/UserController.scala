19e3f828a0f1b49495d7b5afba0972c350edf815
Давид <chaava01@gmail.com>
2018-06-23 19:09:46 +0300

    add delete by id method

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -9,23 +9,21 @@ import play.api.mvc.{AbstractController, ControllerComponents}
 import scala.concurrent.Future
 import services.Implicits._
 
-class UserController @Inject()(userDao: UserDao,
-                               cc: ControllerComponents) extends AbstractController(cc) {
+class UserController @Inject()(userDao: UserDao, cc: ControllerComponents)
+    extends AbstractController(cc) {
 
   import UserDao.parser
   import scala.concurrent.ExecutionContext.Implicits.global
 
   private val contentTypeJson = "application/json"
 
-  def getAll() = Action.async {
-    implicit rq =>
-
-      Future {
-        val limit: Option[String] = rq.getQueryString("limit")
-        val offset: Option[String] = rq.getQueryString("offset")
-        val users: List[User] = userDao.findAll(limit, offset)
-        Ok(Json.toJson(users)).as(contentTypeJson)
-      }
+  def getAll() = Action.async { implicit rq =>
+    Future {
+      val limit: Option[String] = rq.getQueryString("limit")
+      val offset: Option[String] = rq.getQueryString("offset")
+      val users: List[User] = userDao.findAll(limit, offset)
+      Ok(Json.toJson(users)).as(contentTypeJson)
+    }
   }
 
   def getById(id: Long) = Action.async {
